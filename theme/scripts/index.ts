e837e6de79a32942983392843833ae1f42c7cd2e
Christine Kim <chrismekim@gmail.com>
2021-01-28 17:24:18 -0600

    Add mobile menu dropdown

--- theme/scripts/index.ts
+++ theme/scripts/index.ts
@@ -1,1 +1,17 @@
 import "../styles/index.scss";
+const navMenuButton = document.getElementById('nav-menu-button');
+const navCloseMenuButton = document.getElementById('nav-close-menu-button');
+const navMobileMenu = document.getElementById('nav-mobile-menu');
+
+const openMenu = () => {
+  navMobileMenu.classList.remove('opacity-0', 'events-none');
+  navMobileMenu.classList.add('opacity-1', 'events-all');
+};
+
+const closeMenu = () => {
+  navMobileMenu.classList.remove('opacity-1', 'events-all');
+  navMobileMenu.classList.add('opacity-0', 'events-none');
+};
+
+navMenuButton.addEventListener('click', openMenu);
+navCloseMenuButton.addEventListener('click', closeMenu);
