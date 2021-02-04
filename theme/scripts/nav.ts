a497a04bfdcda46cc508a12f2974d5de6e2635a7
Christine <43737723+chrismekim@users.noreply.github.com>
2021-02-04 12:58:24 -0600

    First pass at Nav (#50)
    * Add basic nav
    
    * Adjust mobile styling
    
    * Fix link
    
    * Add mobile menu dropdown
    
    * In progress
    
    * Fix img urls
    
    * Add to language file
    
    * Clean up
    
    * Clean up
    
    * Clean up
    
    * Clean up liquid logic for checking if last of array

---
+++ theme/scripts/nav.ts
@@ -0,0 +1,14 @@
+const navOpenMenuButton = document.getElementById('nav-open-menu-button');
+const navCloseMenuButton = document.getElementById('nav-close-menu-button');
+const navMobileMenu = document.getElementById('nav-mobile-menu');
+
+const openMenu = () => {
+  navMobileMenu.classList.add('MobileNav__menu--active');
+};
+
+const closeMenu = () => {
+  navMobileMenu.classList.remove('MobileNav__menu--active');
+};
+
+navOpenMenuButton.addEventListener('click', openMenu);
+navCloseMenuButton.addEventListener('click', closeMenu);
