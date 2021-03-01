a921e4e8baede29ed19421e73d1db64bcd50961a
Sebastian Odell <sebastian@sanctuary.computer>
2021-03-01 12:28:10 -0500

    Use activatable logic (#62)
    * Adds activatable
    
    * Imports activatable and removes nav.ts
    
    * Updates nav.liquid and nav.scss

---
+++ theme/scripts/activatable.ts
@@ -0,0 +1,73 @@
+export default(function() {
+  const Activatable = {
+    SELECTORS: {
+      activatable: "[data-activatable]",
+      /* Trigger that can activate and deactivate an activatable */
+      toggle: "[data-activatable-toggle]",
+      /* Trigger that can only activate an activatable */
+      activateTrigger: "[data-activatable-activate-trigger]",
+      /* Trigger that can only deactivate an activatable */
+      deactivateTrigger: "[data-activatable-deactivate-trigger]",
+      active: "data-active",
+    },
+
+    init() {
+      const activatables = [].slice.call(
+        document.querySelectorAll(Activatable.SELECTORS.activatable)
+      );
+
+      Activatable.bindEventListeners(activatables);
+    },
+
+    bindEventListeners(activatables: Element[]) {
+      activatables.forEach((activatable) => {
+        const toggle = activatable.querySelector(Activatable.SELECTORS.toggle);
+        const activateTrigger =  activatable.querySelector(Activatable.SELECTORS.activateTrigger);
+        const deactivateTrigger =  activatable.querySelector(Activatable.SELECTORS.deactivateTrigger);
+
+        if (toggle) {
+          toggle.addEventListener("click", (e) => {
+            e.stopPropagation();
+            Activatable.toggle(activatable);
+          });
+        }
+
+        if (activateTrigger) {
+          activateTrigger.addEventListener("click", (e) => {
+            e.stopPropagation();
+            Activatable.activate(activatable);
+          });
+        }
+
+        if (deactivateTrigger) {
+          deactivateTrigger.addEventListener("click", (e) => {
+            e.stopPropagation();
+            Activatable.deactivate(activatable);
+          });
+        }
+      });
+    },
+
+    isActive(activatable: Element) {
+      return activatable.hasAttribute(Activatable.SELECTORS.active);
+    },
+
+    toggle(activatable: Element) {
+      if (Activatable.isActive(activatable)) {
+        Activatable.deactivate(activatable);
+      } else {
+        Activatable.activate(activatable);
+      }
+    },
+
+    deactivate(activatable: Element) {
+      activatable.removeAttribute(Activatable.SELECTORS.active);
+    },
+
+    activate(activatable: Element) {
+      activatable.setAttribute(Activatable.SELECTORS.active, "true");
+    },
+  };
+
+  Activatable.init();
+})();
