45059c6b84e8016445507505358bfed7b86f6016
Sebastian Odell <sebastian@sanctuary.computer>
2021-03-11 10:25:37 -0500

    cleanup

--- theme/scripts/index.ts
+++ theme/scripts/index.ts
@@ -1,5 +1,6 @@
 import * as Sentry from "@sentry/browser";
 import { Integrations } from "@sentry/tracing";
+import 'what-input'
 
 Sentry.init({
   dsn: "https://3ba2a9032f084077bdaad6b264ede016@o462346.ingest.sentry.io/5668673",
