8c5271906bac2dfcb0284355c7efaf0909bd259d
Sebastian Odell <sebastian@sanctuary.computer>
2021-03-16 15:50:18 -0400

    Addresses QA (#119)

--- theme/scripts/index.ts
+++ theme/scripts/index.ts
@@ -14,4 +14,5 @@ import "./productImagesSlider";
 import "./cart";
 import "./activatable";
 import "./cookieConsent";
-import './emailSubscribe';
+import "./emailSubscribe";
+import "./logo";
---
+++ theme/scripts/logo.ts
@@ -0,0 +1,42 @@
+import getRandomInt from './utils/getRandomInt';
+
+// Alphabet array, minus characters in 'Index'
+const ALPHABET = ['a', 'b', 'c', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'];
+
+export default (function() {
+  const Logo = {
+    SELECTORS: {
+      logoText: "[data-logo-text]",
+    },
+    CONSTANTS: {
+      index: 'index'
+    },
+
+    init() {
+      const logoEl: HTMLElement = document.querySelector(Logo.SELECTORS.logoText);
+      Logo.bindEventListeners(logoEl);
+    },
+
+    bindEventListeners(logoEl: HTMLElement) {
+      logoEl.addEventListener('mouseenter', () => Logo.randomizeLogo(logoEl));
+      logoEl.addEventListener('mouseleave', () => Logo.resetLogo(logoEl))
+    },
+
+    randomizeLogo(logoEl: HTMLElement) {
+      const randomCharIndex = getRandomInt(1, 3);
+      const randomAlphabetIndex = getRandomInt(0, ALPHABET.length - 1);
+
+      const indexArr = (Logo.CONSTANTS.index).split("");
+      indexArr[randomCharIndex] = ALPHABET[randomAlphabetIndex];
+
+      logoEl.innerHTML = indexArr.join("");
+    },
+
+    resetLogo(logoEl: HTMLElement) {
+      logoEl.innerHTML = Logo.CONSTANTS.index;
+    }
+  };
+
+  Logo.init();
+})();
+
