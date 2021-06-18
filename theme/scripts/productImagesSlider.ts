1fdb7bc3c5feaa15cfb057bf31639924c7eb7cdc
Christine Kim <chrismekim@gmail.com>
2021-06-18 14:43:13 -0700

    Add dragger

--- theme/scripts/productImagesSlider.ts
+++ theme/scripts/productImagesSlider.ts
@@ -16,10 +16,6 @@ export default(function() {
 
 		setup(slider: HTMLElement) {
 			new Splide(slider, {
-				classes: {
-					arrows: 'splide__arrows--override',
-					arrow : 'splide__arrow--override',
-				},
 				speed: 800,
 				type: 'loop',
 				pagination: false,
