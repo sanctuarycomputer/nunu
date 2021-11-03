b3afbcb00ecf125ef409ec2ecfa511d257be0e34
Devin Halladay <devin@devinhalladay.com>
2021-11-03 00:21:20 -0400

    Setup slider with noUiSlider. Link to patreon URLs. Add copywriting.

--- theme/scripts/patronSlider.ts
+++ theme/scripts/patronSlider.ts
@@ -1,47 +1,84 @@
+import * as rangeSlider from "nouislider";
+
 export default (function () {
   const PatronSlider = {
     SELECTORS: {
-      rangeContainer: "[data-range-container]",
-      range: "[data-range-input]",
-      bubble: "[data-range-output]",
-      outputValue: "[data-range-output-value]"
+      sliderContainer: "[data-patron-slider-container]",
+      handle: ".noUi-handle",
+      patreonLink: "[data-patreon-link]",
+      copy: "[data-patreon-copy]"
     },
-    STATE: {
-      setIntervalId: null,
+    START_VALUE: 5,
+    PATREON_LEVELS: {
+      5: {
+        href: 'https://www.patreon.com/join/index_space/checkout?rid=7795091',
+        copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public."
+      },
+      10: {
+        href: 'https://www.patreon.com/join/index_space/checkout?rid=7795092',
+        copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public. You'll also support our ongoing programming at Index."
+      },
+      15: {
+        href: 'https://www.patreon.com/join/index_space/checkout?rid=7849151',
+        copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public. You'll also support our ongoing programming at Index."
+      },
+      20: {
+        href: 'https://www.patreon.com/join/index_space/checkout?rid=7836923',
+        copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public. You'll also help sustain our programming at Index."
+      },
+      25: {
+        href: 'https://www.patreon.com/join/index_space/checkout?rid=7849152',
+        copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public. You'll also help sustain our programming at Index."
+      },
     },
 
     init() {
-      const rangeContainer: HTMLElement = document.querySelector(
-        PatronSlider.SELECTORS.rangeContainer
-      );
-      const range: HTMLElement = rangeContainer.querySelector(
-        PatronSlider.SELECTORS.range
-      );
-      const bubble: HTMLElement = rangeContainer.querySelector(
-        PatronSlider.SELECTORS.bubble
-      );
-      PatronSlider.bindEventListeners(range, bubble);
-      PatronSlider.setBubble(range, bubble);
-    },
+      const slider = rangeSlider.create(
+        document.querySelector(PatronSlider.SELECTORS.sliderContainer),
+        {
+          start: [PatronSlider.START_VALUE],
+          connect: true,
+          range: {
+            min: PatronSlider.START_VALUE,
+            max: 25,
+          },
+          step: 5,
+        }
+      )
 
-    bindEventListeners(range: HTMLElement, bubble: HTMLElement) {
-      range.addEventListener("input", () => {
-        PatronSlider.setBubble(range, bubble);
+
+      document
+        .querySelector(PatronSlider.SELECTORS.handle)
+        .setAttribute("data-before", `$${PatronSlider.START_VALUE}`);
+
+      document
+        .querySelector(PatronSlider.SELECTORS.patreonLink)
+        .setAttribute("href", PatronSlider.PATREON_LEVELS[PatronSlider.START_VALUE].href);
+
+      document
+        .querySelector(PatronSlider.SELECTORS.copy).textContent = PatronSlider.PATREON_LEVELS[PatronSlider.START_VALUE].copy
+
+      document
+        .querySelector(PatronSlider.SELECTORS.handle)
+        .setAttribute("data-after", `Monthly`);
+
+      slider.on("update", function (event) {
+        const value = parseInt(event.toString())
+
+        document
+          .querySelector(PatronSlider.SELECTORS.handle)
+          .setAttribute("data-before", `$${value}`);
+
+        document
+          .querySelector(PatronSlider.SELECTORS.copy).textContent = PatronSlider.PATREON_LEVELS[value].copy
+
+        document
+          .querySelector(PatronSlider.SELECTORS.patreonLink)
+          .setAttribute("href", PatronSlider.PATREON_LEVELS[value].href);
       });
     },
 
-    setBubble(range, bubble) {
-      const val = range.value;
-      const min = range.min ? range.min : 0;
-      const max = range.max ? range.max : 100;
-      const newVal = Number(((val - min) * 100) / (max - min));
-      bubble.querySelector(
-        PatronSlider.SELECTORS.outputValue
-      ).innerHTML = val;
-
-      // Sorta magic numbers based on size of the native UI thumb
-      bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
-    },
+    bindEventListeners() {},
   };
 
   PatronSlider.init();
