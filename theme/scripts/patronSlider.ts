0bfa23d363de6cd85da661df8e36896760d77532
Devin Halladay <devin@devinhalladay.com>
2021-11-03 23:03:18 -0400

    Cleanup and bugfixes

--- theme/scripts/patronSlider.ts
+++ theme/scripts/patronSlider.ts
@@ -1,4 +1,5 @@
 import * as rangeSlider from "nouislider";
+import getNextSibling from "./utils/getNextSibling";
 
 export default (function () {
   const PatronSlider = {
@@ -8,57 +9,47 @@ export default (function () {
       patreonLink: "[data-patreon-link]",
       copy: "[data-patreon-copy]",
       patreonTier: "[data-patreon-tier]",
-      pips: ".noUi-marker"
+      pips: ".noUi-marker",
+      pipsValue: ".noUi-value",
     },
     START_VALUE: 15,
+    MONTHLY: "Monthly",
     PATREON_LEVELS: {
-      '5': {
-        href: 'https://www.patreon.com/join/index_space/checkout?rid=7795091',
+      "5": {
+        href: "https://www.patreon.com/join/index_space/checkout?rid=7795091",
         copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public.",
-        patreonTier: "Community Member"
+        patreonTier: "Community Member",
       },
-      '10': {
-        href: 'https://www.patreon.com/join/index_space/checkout?rid=7795092',
+      "10": {
+        href: "https://www.patreon.com/join/index_space/checkout?rid=7795092",
         copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public. You'll also support our ongoing programming at Index.",
-        patreonTier: "Supporter Member"
+        patreonTier: "Supporter Member",
       },
-      '15': {
-        href: 'https://www.patreon.com/join/index_space/checkout?rid=7849151',
+      "15": {
+        href: "https://www.patreon.com/join/index_space/checkout?rid=7849151",
         copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public. You'll also support our ongoing programming at Index.",
-        patreonTier: "Supporter Member+"
+        patreonTier: "Supporter Member+",
       },
-      '20': {
-        href: 'https://www.patreon.com/join/index_space/checkout?rid=7836923',
+      "20": {
+        href: "https://www.patreon.com/join/index_space/checkout?rid=7836923",
         copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public. You'll also help sustain our programming at Index.",
-        patreonTier: "Sustaining Member"
+        patreonTier: "Sustaining Member",
       },
-      '25': {
-        href: 'https://www.patreon.com/join/index_space/checkout?rid=7849152',
+      "25": {
+        href: "https://www.patreon.com/join/index_space/checkout?rid=7849152",
         copy: "You'll get access to our community Discord, Arena, Time Exchange, member-only events, and our archived talks earlier than the public. You'll also help sustain our programming at Index.",
-        patreonTier: "Sustaining Member+"
+        patreonTier: "Sustaining Member+",
       },
     },
 
-    getNextSibling(elem, selector) {
-      var sibling = elem.nextElementSibling;
-
-      // If there's no selector, return the first sibling
-      if (!selector) return sibling;
-
-      // If the sibling matches our selector, use it
-      // If not, jump to the next sibling and continue the loop
-      while (sibling) {
-        if (sibling.matches(selector)) return sibling;
-        sibling = sibling.nextElementSibling
-      }
-    },
+    SLIDER: null,
 
     init() {
-      const slider = rangeSlider.create(
+      PatronSlider.SLIDER = rangeSlider.create(
         document.querySelector(PatronSlider.SELECTORS.sliderContainer),
         {
           start: [PatronSlider.START_VALUE],
-          step: 5,
+          step: Object.keys(PatronSlider.PATREON_LEVELS).length,
           range: {
             min: 5,
             max: 25,
@@ -66,67 +57,84 @@ export default (function () {
           pips: {
             mode: rangeSlider.PipsMode.Steps,
             filter: (value) => {
-              if ([5,10,15,20,25].indexOf(value) > -1) {
+              if ([5, 10, 15, 20, 25].indexOf(value) > -1) {
                 return 1;
               }
-              return -1
-            }
-          }
+              return -1;
+            },
+          },
         }
-      )
+      );
 
-      const pips = document.querySelectorAll(PatronSlider.SELECTORS.pips)
+      const handle = document.querySelector(PatronSlider.SELECTORS.handle);
+      const patreonLink = document.querySelector(
+        PatronSlider.SELECTORS.patreonLink
+      );
+      const tierCopywriting = document.querySelector(
+        PatronSlider.SELECTORS.copy
+      );
+      const patreonTierName = document.querySelector(
+        PatronSlider.SELECTORS.patreonTier
+      );
 
+      handle.setAttribute("data-before", `$${PatronSlider.START_VALUE}`);
+
+      patreonLink.setAttribute(
+        "href",
+        PatronSlider.PATREON_LEVELS[PatronSlider.START_VALUE].href
+      );
+
+      tierCopywriting.textContent =
+        PatronSlider.PATREON_LEVELS[PatronSlider.START_VALUE].copy;
+
+      patreonTierName.textContent =
+        PatronSlider.PATREON_LEVELS[PatronSlider.START_VALUE].patreonTier;
+
+      handle.setAttribute("data-after", PatronSlider.MONTHLY);
+
+      PatronSlider.bindEventListeners();
+    },
+
+    bindEventListeners() {
+      const pips = document.querySelectorAll(PatronSlider.SELECTORS.pips);
+      const handle = document.querySelector(PatronSlider.SELECTORS.handle);
+      const patreonLink = document.querySelector(
+        PatronSlider.SELECTORS.patreonLink
+      );
+      const tierCopywriting = document.querySelector(
+        PatronSlider.SELECTORS.copy
+      );
+      const patreonTierName = document.querySelector(
+        PatronSlider.SELECTORS.patreonTier
+      );
+
+      // Handle clicks on "pips", or notches, on the slider.
       const handlePipClick = (e) => {
-        console.log(e);
+        const valueEl = getNextSibling(
+          e.target,
+          PatronSlider.SELECTORS.pipsValue
+        );
+        var value = Number(valueEl.getAttribute("data-value"));
+        PatronSlider.SLIDER.set(value);
+      };
 
-        const valueEl = PatronSlider.getNextSibling(e.target, '.noUi-value')
-        var value = Number(valueEl.getAttribute('data-value'));
-        slider.set(value);
-      }
-
-      document
-        .querySelector(PatronSlider.SELECTORS.handle)
-        .setAttribute("data-before", `$${PatronSlider.START_VALUE}`);
-
-      document
-        .querySelector(PatronSlider.SELECTORS.patreonLink)
-        .setAttribute("href", PatronSlider.PATREON_LEVELS[PatronSlider.START_VALUE].href);
-
-      document
-        .querySelector(PatronSlider.SELECTORS.copy).textContent = PatronSlider.PATREON_LEVELS[PatronSlider.START_VALUE].copy
-
-      document
-        .querySelector(PatronSlider.SELECTORS.patreonTier).textContent = PatronSlider.PATREON_LEVELS[PatronSlider.START_VALUE].patreonTier
-
-      document
-        .querySelector(PatronSlider.SELECTORS.handle)
-        .setAttribute("data-after", `Monthly`);
-
-      slider.on("update", function (event) {
-        const value = parseInt(event.toString())
-
-        document
-          .querySelector(PatronSlider.SELECTORS.handle)
-          .setAttribute("data-before", `$${value}`);
-
-        document
-          .querySelector(PatronSlider.SELECTORS.copy).textContent = PatronSlider.PATREON_LEVELS[value].copy
-
-        document
-          .querySelector(PatronSlider.SELECTORS.patreonTier).textContent = PatronSlider.PATREON_LEVELS[value].patreonTier
-
-        document
-          .querySelector(PatronSlider.SELECTORS.patreonLink)
-          .setAttribute("href", PatronSlider.PATREON_LEVELS[value].href);
+      PatronSlider.SLIDER.on("update", function (event) {
+        const sliderValue = parseInt(event.toString());
+        handle.setAttribute("data-before", `$${sliderValue}`);
+        tierCopywriting.textContent =
+          PatronSlider.PATREON_LEVELS[sliderValue].copy;
+        patreonTierName.textContent =
+          PatronSlider.PATREON_LEVELS[sliderValue].patreonTier;
+        patreonLink.setAttribute(
+          "href",
+          PatronSlider.PATREON_LEVELS[sliderValue].href
+        );
       });
 
       for (var i = 0; i < pips.length; i++) {
-        pips[i].addEventListener('click', (e) => handlePipClick(e));
+        pips[i].addEventListener("click", (e) => handlePipClick(e));
       }
     },
-
-    bindEventListeners() {},
   };
 
   PatronSlider.init();
---
+++ theme/scripts/utils/getNextSibling.ts
@@ -0,0 +1,13 @@
+export default (elem, selector) => {
+  var sibling = elem.nextElementSibling;
+
+  // If there's no selector, return the first sibling
+  if (!selector) return sibling;
+
+  // If the sibling matches our selector, use it
+  // If not, jump to the next sibling and continue the loop
+  while (sibling) {
+    if (sibling.matches(selector)) return sibling;
+    sibling = sibling.nextElementSibling
+  }
+}
