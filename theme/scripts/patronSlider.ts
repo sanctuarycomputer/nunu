5a1315ddd4e57baa8c31f05cf586e2d6db34f45f
Devin Halladay <devin@devinhalladay.com>
2021-11-24 13:49:48 -0500

    Cleanup. Move constants outside of module

--- theme/scripts/patronSlider.ts
+++ theme/scripts/patronSlider.ts
@@ -3,6 +3,43 @@ import getNextSibling from "./utils/getNextSibling";
 
 const TIER_PRICES = [5, 10, 15, 20, 25];
 
+const START_VALUE = 15;
+
+const MONTHLY = "Monthly";
+
+const PATRON_TIERS = {
+  "5": {
+    href: "https://www.patreon.com/join/index_space/checkout?rid=7795091",
+    description:
+      "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public.",
+    name: "Community Patron",
+  },
+  "10": {
+    href: "https://www.patreon.com/join/index_space/checkout?rid=7795092",
+    description:
+      "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public. You'll also support our ongoing programming at Index.",
+    name: "Supporter Patron",
+  },
+  "15": {
+    href: "https://www.patreon.com/join/index_space/checkout?rid=7849151",
+    description:
+      "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public. You'll also support our ongoing programming at Index.",
+    name: "Supporter Patron+",
+  },
+  "20": {
+    href: "https://www.patreon.com/join/index_space/checkout?rid=7836923",
+    description:
+      "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public. You'll also help sustain our programming at Index.",
+    name: "Sustaining Patron",
+  },
+  "25": {
+    href: "https://www.patreon.com/join/index_space/checkout?rid=7849152",
+    description:
+      "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public. You'll also help sustain our programming at Index.",
+    name: "Sustaining Patron+",
+  },
+};
+
 export default (function () {
   const PatronSlider = {
     SELECTORS: {
@@ -14,40 +51,6 @@ export default (function () {
       patronTierDescription: "[data-patron-tier-description]",
       patronTierName: "[data-patron-tier-name]",
     },
-    START_VALUE: 15,
-    MONTHLY: "Monthly",
-    PATRON_TIERS: {
-      "5": {
-        href: "https://www.patreon.com/join/index_space/checkout?rid=7795091",
-        description:
-          "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public.",
-        name: "Community Patron",
-      },
-      "10": {
-        href: "https://www.patreon.com/join/index_space/checkout?rid=7795092",
-        description:
-          "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public. You'll also support our ongoing programming at Index.",
-        name: "Supporter Patron",
-      },
-      "15": {
-        href: "https://www.patreon.com/join/index_space/checkout?rid=7849151",
-        description:
-          "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public. You'll also support our ongoing programming at Index.",
-        name: "Supporter Patron+",
-      },
-      "20": {
-        href: "https://www.patreon.com/join/index_space/checkout?rid=7836923",
-        description:
-          "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public. You'll also help sustain our programming at Index.",
-        name: "Sustaining Patron",
-      },
-      "25": {
-        href: "https://www.patreon.com/join/index_space/checkout?rid=7849152",
-        description:
-          "You'll get access to our community Discord, Arena, Time Exchange, patron-only events, and our archived talks earlier than the public. You'll also help sustain our programming at Index.",
-        name: "Sustaining Patron+",
-      },
-    },
 
     SLIDER: null,
 
@@ -57,9 +60,9 @@ export default (function () {
         document.querySelector(PatronSlider.SELECTORS.slider),
         {
           // Start with the slider at this value.
-          start: [PatronSlider.START_VALUE],
+          start: [START_VALUE],
           // Add this many notches along the slider.
-          step: Object.keys(PatronSlider.PATRON_TIERS).length,
+          step: Object.keys(PATRON_TIERS).length,
           range: {
             min: 5,
             max: 25,
@@ -70,12 +73,7 @@ export default (function () {
             filter: (value) => {
               // If a slider value is not in our list of values,
               // do not render a pip for that value.
-              if (TIER_PRICES.includes(value)) {
-                return 1;
-              } else {
-                // Render a pip.
-                return -1;
-              }
+              return TIER_PRICES.includes(value) ? 1 : -1;
             },
           },
         }
@@ -113,20 +111,15 @@ export default (function () {
       patronTierName,
     }) {
       // Used to set the content of our slider handle
-      handle.setAttribute("data-before", `$${PatronSlider.START_VALUE}`);
+      handle.setAttribute("data-before", `$${START_VALUE}`);
       // Used to set the content of our drag handle.
-      handle.setAttribute("data-after", PatronSlider.MONTHLY);
+      handle.setAttribute("data-after", MONTHLY);
 
-      patronTierLink.setAttribute(
-        "href",
-        PatronSlider.PATRON_TIERS[PatronSlider.START_VALUE].href
-      );
+      patronTierLink.setAttribute("href", PATRON_TIERS[START_VALUE].href);
 
-      tierCopywriting.textContent =
-        PatronSlider.PATRON_TIERS[PatronSlider.START_VALUE].description;
+      tierCopywriting.textContent = PATRON_TIERS[START_VALUE].description;
 
-      patronTierName.textContent =
-        PatronSlider.PATRON_TIERS[PatronSlider.START_VALUE].name;
+      patronTierName.textContent = PATRON_TIERS[START_VALUE].name;
     },
 
     bindEventListeners({
@@ -143,21 +136,16 @@ export default (function () {
 
         handle.setAttribute("data-before", `$${sliderValue}`);
 
-        tierCopywriting.textContent =
-          PatronSlider.PATRON_TIERS[sliderValue].description;
+        tierCopywriting.textContent = PATRON_TIERS[sliderValue].description;
 
-        patronTierName.textContent =
-          PatronSlider.PATRON_TIERS[sliderValue].name;
+        patronTierName.textContent = PATRON_TIERS[sliderValue].name;
 
-        patronTierLink.setAttribute(
-          "href",
-          PatronSlider.PATRON_TIERS[sliderValue].href
-        );
+        patronTierLink.setAttribute("href", PATRON_TIERS[sliderValue].href);
       });
 
       // Listen for clicks on our pips
-      pips.forEach(pip => {
-        pip.addEventListener("click", PatronSlider.handlePipClick)
+      pips.forEach((pip) => {
+        pip.addEventListener("click", PatronSlider.handlePipClick);
       });
     },
 
