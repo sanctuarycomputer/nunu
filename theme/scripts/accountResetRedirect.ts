4953fcca54d5979cff29161644ebde69b849b6a4
uditdesai <udit.desai3@gmail.com>
2022-02-01 11:51:45 -0500

    change redirect route to new members app reset route to mimic shopify

--- theme/scripts/accountResetRedirect.ts
+++ theme/scripts/accountResetRedirect.ts
@@ -1,20 +1,14 @@
 // URL of members reset password page
-const MEMBERS_RESET_PASSWORD_URL = "https://members.index-space.org/reset-password";
+const MEMBERS_RESET_PASSWORD_URL = "https://members.index-space.org";
 // A typical shopify password reset url is like this:
 // https://index-space.org/account/reset/[id]/[token]
 const SHOPIFY_RESET_PASSWORD_PATHNAME = "/account/reset";
-const ID_PATHNAME_POSITION = 3;
-const TOKEN_PATHNAME_POSITION = 4;
 
 export default (function () {
   const AccountResetRedirect = {
     init() {
       if (window.location.pathname.includes(SHOPIFY_RESET_PASSWORD_PATHNAME)) {
-        const pathList = window.location.pathname.split('/');
-        const id = pathList[ID_PATHNAME_POSITION];
-        const token = pathList[TOKEN_PATHNAME_POSITION];
-
-        const redirectUrl = `${MEMBERS_RESET_PASSWORD_URL}?id=${id}&token=${token}`;
+        const redirectUrl = `${MEMBERS_RESET_PASSWORD_URL}${window.location.pathname}`;
         window.location.replace(redirectUrl)
       }
     },
